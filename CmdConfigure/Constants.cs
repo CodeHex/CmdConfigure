548821e4f6ac1d42dd92f442723bbf556417d4dd
Ben Ibinson <codehex.net@gmail.com>
2014-04-16 20:01:01 +0100

    First Commit with initial UI

---
+++ CmdConfigure/Constants.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CmdConfigure
+{
+    public static class Constants
+    {
+        public static int[] FontSizes = new[] { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+
+        public const string ExampleText = "Microsoft Windows [Version 6.1.7601]\n" +
+                                           "Copyright (c) 2009 Microsoft Corporation. All rights reserved.\n\n" +
+                                           "C:\\Users\\Admin>";
+
+        public static List<ConsoleColorEntry> ColorList = new List<ConsoleColorEntry> {
+            new ConsoleColorEntry {Name = "Black", Color = ConsoleColor.Black, Brush = Brushes.Black},
+            new ConsoleColorEntry {Name = "Dark Blue", Color = ConsoleColor.DarkBlue, Brush = Brushes.DarkBlue},
+            new ConsoleColorEntry {Name = "Dark Green", Color = ConsoleColor.DarkGreen, Brush = Brushes.DarkGreen},
+            new ConsoleColorEntry {Name = "Dark Cyan", Color = ConsoleColor.DarkCyan, Brush = Brushes.DarkCyan},
+            new ConsoleColorEntry {Name = "Dark Red", Color = ConsoleColor.DarkRed, Brush = Brushes.DarkRed},
+            new ConsoleColorEntry {Name = "Dark Magenta", Color = ConsoleColor.DarkMagenta, Brush = Brushes.DarkMagenta},
+            new ConsoleColorEntry {Name = "Dark Yellow", Color = ConsoleColor.DarkYellow, Brush = new SolidColorBrush(Color.FromArgb(255,128,128,0))},
+            new ConsoleColorEntry {Name = "Gray", Color = ConsoleColor.Gray, Brush = Brushes.Gray},
+            new ConsoleColorEntry {Name = "Dark Gray", Color = ConsoleColor.DarkGray, Brush = Brushes.DarkGray},
+            new ConsoleColorEntry {Name = "Blue", Color = ConsoleColor.Blue, Brush = Brushes.Blue},
+            new ConsoleColorEntry {Name = "Green", Color = ConsoleColor.Green, Brush = Brushes.LightGreen},
+            new ConsoleColorEntry {Name = "Cyan", Color = ConsoleColor.Cyan, Brush = Brushes.Cyan},
+            new ConsoleColorEntry {Name = "Red", Color = ConsoleColor.Red, Brush = Brushes.Red},
+            new ConsoleColorEntry {Name = "Magenta", Color = ConsoleColor.Magenta, Brush = Brushes.Magenta},
+            new ConsoleColorEntry {Name = "Yellow", Color = ConsoleColor.Yellow, Brush = Brushes.Yellow},
+            new ConsoleColorEntry {Name = "White", Color = ConsoleColor.White, Brush = Brushes.White},
+        };
+    }
+
+    public class ConsoleColorEntry
+    {
+        public string Name { get; set; }
+        public ConsoleColor Color { get; set; }
+        public SolidColorBrush Brush { get; set; }
+    }
+}
---
+++ CmdConfigure/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Drawing.Text;
+
+namespace CmdConfigure
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FontsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ExampleTextBox == null)
+                return;
+
+            var selectedFont = e.AddedItems[0] as FontFamily;
+            ExampleTextBox.FontFamily = selectedFont;
+
+        }
+
+        private void SizeListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ExampleTextBox == null)
+                return;
+
+            var selectedSize = e.AddedItems[0] as int?;
+            ExampleTextBox.FontSize = selectedSize.Value;
+        }
+
+        private void BackgroundColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ExampleTextBox == null)
+                return;
+
+            var backColor = e.AddedItems[0] as ConsoleColorEntry;
+            ExampleTextBox.Background = backColor.Brush;
+        }
+
+        private void ForegroundColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ExampleTextBox == null)
+                return;
+
+            var foreColor = e.AddedItems[0] as ConsoleColorEntry;
+            ExampleTextBox.Foreground = foreColor.Brush;
+        }
+    }
+}
